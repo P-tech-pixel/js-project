f01b394fa31d8b1a6d63c7522d3518b4fab880b6
Prashanta Pandit <57577969+P-tech-pixel@users.noreply.github.com>
2023-12-06 00:39:30 +0000

    Conversion Study

---
+++ 03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score = "33a"
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+
+//convert string to Number
+
+let valueInNumber = Number(score) // convert the string to Number 
+
+let convertToBoolean = Boolean(score)
+console.log(typeof valueInNumber);
+
+
+// If we do conversioon: 
+//"33" = 33
+// "33abc" = NaN
+// true => 1; false => 0
+
