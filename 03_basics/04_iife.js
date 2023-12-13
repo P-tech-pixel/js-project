f62e3972e6b974b8d775a5440114f7b6c03011e6
Prashanta Pandit <57577969+P-tech-pixel@users.noreply.github.com>
2023-12-13 00:30:12 +0000

    03

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -17,4 +17,18 @@
     console.log(`DB CONNECTED 2 BY ${name}`)
 })('Prashanta Pandit');
 
+// How do Java Script Execute Code: reference: https://youtu.be/ByhtOgF6uYM?si=OXUGeR2X-DD4tdts
+
+/* Phase of Code execution in JS.
+
+1. Global execution: allocated on .this keyword. 
+
+2. Memory creation phase: allocate all the memory for the variables. The value of the variables are allocated as Undefined. In this phase, functions
+    value is allocated as a defination. 
+
+3. Execution phase: 
+   This phase, all the variables values are executed according to their variable types. 
+   Once, the function get executed the memory is not saved. 
+
+*/
 
